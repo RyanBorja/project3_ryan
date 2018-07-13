880f4ce79e8b8b80cf96029fb6023c9b88113f7c
Ryan Borja <ryan.t.bortja@gmail.com>
2018-07-13 17:10:53 -0400

    end of day

--- config/database.php
+++ config/database.php
@@ -65,11 +65,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => $dbhost,
-            'port' => $dbport,
-            'database' => $dbname,
-            'username' => $dbuser,
-            'password' => $dbpass,
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
